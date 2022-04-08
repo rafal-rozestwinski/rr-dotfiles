05c855acd5dba40dbaa90a7367f5d4bb522d0e20
Rafal Rozestwinski <rafal@rozestwinski.com>
2022-04-08 13:10:45 +0200

    global

---
+++ provisioning/node/global.sh
@@ -0,0 +1,7 @@
+# pure-js:
+npm install -g ts-node typescript glob ts-node  tslog tslib @types/node gulp-cli
+
+npm install -g @awaitjs/express webpack javascript-obfuscator
+
+#native:
+npm install -g ffi-napi serialport bindings cmake-js sqlite3 sqlite serial  serialport
