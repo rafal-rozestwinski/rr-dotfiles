a624597c6b49221756598687309ba8e1229cf9e6
Rafal Rozestwinski <rafal@rozestwinski.com>
2022-06-02 18:54:47 +0200

    installsh

---
+++ provisioning/wyse/install.sh
@@ -0,0 +1,9 @@
+apt install gcc g++ clang gdb valgrind make cmake nodejs golang \
+openssl curl libcurl4-openssl-dev libuv1-dev python3-pip python3-setuptools python3-serial screen tmux vim ncdu lsof htop avahi-daemon \
+tcc nasm yasm ffmpeg mplayer sshfs rsync wireguard testdisk zstd sudo \
+patch git fakeroot libffi-dev faketime libfaketime automake dialog doxygen \
+ninja-build pkg-config libboost-all-dev libxml2-dev libjpeg-dev libsdl2-dev libunwind-dev libsqlite3-dev sqlite3 libspdlog-dev xxd \
+cpulimit net-tools rdiff-backup fuse vlock socat tcpdump nethogs netcat pigz bridge-utils bsdutils links jq strace telnet yapf3 apache2-utils oathtool liboath0 liboath-dev \
+qrencode ethtool libxxhash-dev xxhash ssss cryptsetup util-linux \
+testdisk busybox-static xz-utils zstd gnupg ntfs-3g samba-common  samba-libs smbclient musl-dev musl-tools musl \
+bind9-dnsutils lshw hdparm strace lsof ethtool bind9-utils
